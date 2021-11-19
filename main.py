965a64bf55fc8485a9df4bd854f904046c533b6b
Muralidhara Bhat K S <ksmuralidhara0@gmail.com>
2021-11-19 18:16:28 +0530

    Inital Updates

---
+++ main.py
@@ -0,0 +1,12 @@
+# function to add two numbers and return the sum
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
