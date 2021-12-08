cb33a8a46b2897cb9a34693cce87a69465af0f4d
Muralidhara Bhat K S <ksmuralidhara0@gmail.com>
2021-12-08 18:56:15 +0530

    Updated the main module

--- main.py
+++ main.py
@@ -28,11 +28,11 @@ def dark_mode():
 
 
 # Function to do RSI strategy
-def rsi(val, neutral, window):
+def rsi(value, neutral, window):
 	"""
 
 	Input:
-	:param val: (type dataframe pandas): Entry values of the stock
+	:param value: (type dataframe pandas): Entry values of the stock
 	:param neutral:(float) Value of the neutrality, i.e No Action Zone
 	:param window: (float) rolling period for RSI calculation
 
@@ -40,57 +40,117 @@ def rsi(val, neutral, window):
 	:return:
 	"""
 	# Print error is no Adj Close column is not present
-	if "Adj Close" not in val.columns:
+	if "Adj Close" not in value.columns:
 		ValueError("Error: No Adj Close column is present")
 
-	val['rsi'] = ta.momentum.RSIIndicator(val['Adj Close'], window=window).rsi()
+	value['rsi'] = ta.momentum.RSIIndicator(value['Adj Close'], window=window).rsi()
 
 	"""Long Buy signal"""
 	overbuy = 70
 	neutral_buy = 50 + neutral
 
 	# Put nan values for te signal long columns
-	val['signal_long'] = np.nan
-	val['yesterday_rsi'] = val['rsi'].shift(1)
+	value['signal_long'] = np.nan
+	value['yesterday_rsi'] = value['rsi'].shift(1)
 	# We need to define open long signal (Rsi yes>55 and RSI today>55)
-	val.loc[(val['rsi'] > neutral_buy) & (val['yesterday_rsi'] < neutral_buy), 'signal_long'] = 1
+	value.loc[(value['rsi'] > neutral_buy) & (value['yesterday_rsi'] < neutral_buy), 'signal_long'] = 1
 	# open long signal for false positive (Rsi yes>55 and RSI today<55)
-	val.loc[(val['rsi'] < neutral_buy) & (val['yesterday_rsi'] > neutral_buy), 'signal_long'] = 0
+	value.loc[(value['rsi'] < neutral_buy) & (value['yesterday_rsi'] > neutral_buy), 'signal_long'] = 0
 	# open long signal for overbuy (Rsi yes>70 and RSI today<70)
-	val.loc[(val['rsi'] < overbuy) & (val['yesterday_rsi'] > overbuy), 'signal_long'] = 0
+	value.loc[(value['rsi'] < overbuy) & (value['yesterday_rsi'] > overbuy), 'signal_long'] = 0
 
 	"""Short Sell signal"""
 	oversell = 30
 	neutral_sell = 50 - neutral
 
 	# Put the values for the short signal columns
-	val['signal_short'] = np.nan
+	value['signal_short'] = np.nan
 
 	# We define the Open short signal (RSI yes>45 and RSI today<45)
-	val.loc[(val['rsi'] < neutral_sell) & (val['yesterday_rsi'] > neutral_sell), 'signal_short'] = -1
+	value.loc[(value['rsi'] < neutral_sell) & (value['yesterday_rsi'] > neutral_sell), 'signal_short'] = -1
 
 	# We define the Close short signal(Rsi yes<45 and RSI today>45) False signal
-	val.loc[(val['rsi'] > neutral_sell) & (val['yesterday_rsi'] < neutral_sell), 'signal_short'] = 0
+	value.loc[(value['rsi'] > neutral_sell) & (value['yesterday_rsi'] < neutral_sell), 'signal_short'] = 0
 
 	# We define the Close short signal (RSI yes<30 and RSI today>30) Oversell
-	val.loc[(val['rsi'] < oversell) & (val['yesterday_rsi'] > oversell), 'signal_short'] = 0
+	value.loc[(value['rsi'] < oversell) & (value['yesterday_rsi'] > oversell), 'signal_short'] = 0
 
 	"""Compute the returns"""
 	# Compute the percentage of the variation of the stock
-	val['pct'] = val['Adj Close'].pct_change(1)
+	value['pct'] = value['Adj Close'].pct_change(1)
 
 	# Calculate the position
-	val['Position'] = (val['signal_short'].fillna(method='ffill') + val['signal_long'].fillna(method='ffill'))
+	value['Position'] = (value['signal_short'].fillna(method='ffill') + value['signal_long'].fillna(method='ffill'))
 
 	# Compute the returns
-	val['return'] = val['pct'] * (val['Position'].shift(1))
+	value['return'] = value['pct'] * (value['Position'].shift(1))
 
-	return val['return']
+	return value['return']
+
+
+def drawdown_function(series):
+	cumulative_sum = series.dropna().cumsum() + 1  # multiplication coefficient
+	# compute the max of the cumulative_sum
+	# (1,3,1,1,7,1,2,) -> (1,3,3,3,7,7,7)
+	running_max = np.maximum.accumulate(cumulative_sum)
+	drawdowned = cumulative_sum / running_max - 1
+	return drawdowned
+
+
+def backTest(series):
+	# Import the Bechmark
+	sp500 = yf.download('^GSPC')['Adj Close'].pct_change()
+	sp500.name = 'SP500'
+
+	# We concatenate the two dataframe
+	value = pd.concat([returns, sp500], axis=1).dropna()
+
+	# compute the drawdown and max drawdown
+	drawdown = drawdown_function(series)
+	max_drawdown = -np.min(drawdown)
+
+	"""Plot the Graph"""
+	fig, (cum, draw) = plt.subplots(1, 2, figsize=(20, 6))
+	fig.suptitle("Backtesting", size=20)
+
+	cum.plot(series.cumsum(), color='#39B3C7')
+	cum.plot(value['SP500'].cumsum(), color='#B85A0F')
+
+	cum.legend(["Portfolio", "Benchmark"])
+	cum.set_title("Cumulative Returns", size=13)
+	cum.set_ylabel("Cumulative Return in %", size=13)
+
+	# Plot the drawdown
+	draw.fill_between(drawdown.index, 0, drawdown * 100, color='#C73954', alpha=0.65)
+
+	draw.set_title("Drawdown", size=15)
+	draw.set_ylabel("Drawdown in %", size=13)
+
+	# plot the graph
+	plt.show()
+
+	"""Compute the statistics"""
+	# compute the sortino
+	sortino = np.sqrt(252) * series.mean() / series.loc[series < 0].std()
+
+	# compute the beta
+	beta = np.cov(value[['return', 'SP500']].values, rowvar=False)[0][1] / np.var(value['SP500'].values)
+
+	# compute the alpha
+	alpha = 252 * (series.mean() - beta * series.mean())
+
+	# Print the statistics
+
+	print(f'Sortino: {np.round(sortino, 3)}')
+	print(f'Beta: {np.round(beta, 3)}')
+	print(f'Alpha: {np.round(alpha, 3)}')
+	print(f'Sortino: {np.round(max_drawdown, 3)}')
 
 
 # import the data
-data = yf.download('GOOG')
+data = yf.download('AAPL')
 
 # test
-returns = rsi(data['2010'], 5, 14)
+returns = rsi(data, 5, 14)
 returns.cumsum().plot(figsize=(15, 8))
+backTest(returns)
